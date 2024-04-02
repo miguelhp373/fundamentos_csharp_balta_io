e4cee8c51ba893f64e961fe07b868c760679bcb9
Miguel Henrique <miguelhp373@gmail.com>
2024-04-01 22:57:44 -0300

    add - first project with dotnet

---
+++ sample-microsoft-project/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace sample_microsoft_project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstNumber;
+        string secondNumber;
+        string isOperation;
+
+        firstNumber =  Console.ReadLine()!;
+        secondNumber =  Console.ReadLine()!;
+        isOperation = "minus";
+
+        Program program = new Program(); 
+
+        Console.WriteLine(program.CalculateNumbers(firstNumber, secondNumber, isOperation).ToString());
+    }
+
+    public float CalculateNumbers(string NumberX, string NumberY, string CalcType)
+    {
+        float outPutNumberX;
+        if(!float.TryParse(NumberX.ToString(), out outPutNumberX)){
+            Console.WriteLine("Error! The First Number Is Not Valid!");
+            return 0;
+        }
+
+        float outPutNumberY;
+        if(!float.TryParse(NumberY.ToString(), out outPutNumberY)){
+            Console.WriteLine("Error! The Second Number Is Not Valid!");
+            return 0;
+        }        
+
+        switch (CalcType)
+        {
+            case "plus" :  return outPutNumberX + outPutNumberY;
+            case "minus" :  return outPutNumberX - outPutNumberY;
+            case "division" :  return outPutNumberX / outPutNumberY;
+            case "multiplication" :  return outPutNumberX * outPutNumberY;            
+            default: return 0;
+        }
+    }
+}
---
+++ sample-microsoft-project/Readme.md
@@ -0,0 +1,11 @@
+# Sample Microsoft Project
+
+Create a Project
+```powershell
+dotnet new console --framework net8.0 --use-program-main
+```
+
+Startup Project
+```powershell
+dotnet run
+```
